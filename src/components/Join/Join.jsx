96a2ac53b946ae34716c9a7831d5b8e60f4cfe5d
Anshuman <sharmaaanshumaan@gmail.com>
2023-04-22 22:10:11 +0530

    Modified JOIN UI🛬

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Join.scss';
 import { Link } from 'react-router-dom';
+import { WechatOutlined } from '@ant-design/icons';
 
 const Join = () => {
     const [user, setMyUser] = useState("");
@@ -8,8 +9,14 @@ const Join = () => {
   return (
     <div className="joinContainer">
         <div className="joinCenterBox">
-            <h1>Chat Poppins</h1>
+            <h2 className='appName'>Pop Ups <WechatOutlined style={{
+              fontSize : "25px"
+            }} /></h2>
+            <p className='liner'>(friends/family chats room)</p>
+            <div className="inputBlock">
             <input type="text" name='lo' id='joinInput' onChange={(e) => setMyUser(e.target.value)} required/>
+            <span className='inputHolder'>NAME</span>
+            </div>
             <Link onClick={(e) => !user ? e.preventDefault() : null} to={`/chat`} state={{
                 user : user
             }}>
