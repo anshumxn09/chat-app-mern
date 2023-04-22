6cb1cc21dade62286646e37691dbefcf16278311
Anshuman <sharmaaanshumaan@gmail.com>
2023-04-22 19:41:09 +0530

    modified app components⚔

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
 
 const App = () => {
+
   return (
-    <div></div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={
+          <Join />
+        }></Route>
+
+        <Route exact path='/chat' element={
+          <Chat />
+        }></Route>
+
+      </Routes>
+    </Router>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,5 @@ import { Socket } from 'socket.io-client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
