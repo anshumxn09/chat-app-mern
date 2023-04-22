659016669269003435004ff84b8780dda1e38254
Anshuman <sharmaaanshumaan@gmail.com>
2023-04-22 19:40:44 +0530

    socket server🔱

--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const socketIO = require('socket.io');
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 const app = express();
+
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -12,12 +13,31 @@ app.get("/", (req, res) => {
         message : "God is GREAT!!"
     })
 })
+let users = [{}];
 
 const server = http.createServer(app);
 const io = socketIO(server);
 
-io.on('connection', () => {
-    console.log("New Connection");
+io.on('connection', (socket) => {
+
+    socket.on("joined", ({user}) => {
+        users[socket.id] = user;
+        socket.broadcast.emit("userJoined", {user : "Admin", message : `${users[socket.id]} has joined the chat`});
+
+        socket.emit('welcome', {user : "Admin", message : `Welcome to the CHAT ${users[socket.id]}`});
+    })
+
+    socket.on("disconnected", () => {
+        socket.broadcast.emit("left", {user : "Admin", message : `${users
+        [socket.id]} has left the chat`});
+    })
+
+    socket.on("message", ({message, id}) => {
+        io.emit("sendMessage", {
+            user : users[id],
+            message, id
+        })
+    })
 })
 
 server.listen(port, () => {
